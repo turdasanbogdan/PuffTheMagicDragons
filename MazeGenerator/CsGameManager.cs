c5ffe00af6f5614e8f2efd91cb61e78d6ef1cd49
Bogdan Turdasan <bogdanturdasan@bogdans-mbp.home>
2020-04-30 18:25:23 +0300

    added maze generator

---
+++ MazeGenerator/CsGameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CsGameManager : MonoBehaviour
+{
+	// public intVector2 iSize;
+	// public Cs_MazeCell mazeCellPrefab;
+	// private Cs_MazeCell mazeCellInstance;
+	// private Cs_MazeCell[] cells;
+
+	public Cs_Maze mazePrefab;
+	private Cs_Maze mazeInstance;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        BeginGame();
+    }
+
+    // Update is called once per frame
+    private void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			RestartGame();
+		}
+	}
+
+    private void BeginGame(){
+
+    	mazeInstance = Instantiate(mazePrefab) as Cs_Maze;	
+    	StartCoroutine(mazeInstance.Generate());
+    }
+
+    private void RestartGame(){
+    	StopAllCoroutines();
+    	Destroy(mazeInstance.gameObject);
+    	BeginGame();
+    }
+}
---
+++ MazeGenerator/Cs_MazeDirection.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MazeDirection {
+    North,
+    East,
+    South,
+    West
+}
+
+public static class MazeDirections {
+    public const int Count = 4;
+
+    public static MazeDirection RandomValue {
+        get{
+            return(MazeDirection)Random.Range(0, Count);
+        }
+    }
+
+    private static intVector2[] vectors = {
+        new intVector2(0,1),
+        new intVector2(1,0),
+        new intVector2(0,-1),
+        new intVector2(-1,0)
+
+    };
+
+    public static intVector2 toIntVector2 (this MazeDirection direction){
+        return vectors[(int)direction];
+    } 
+
+  private static MazeDirection[] opposites = {
+		MazeDirection.South,
+		MazeDirection.West,
+		MazeDirection.North,
+		MazeDirection.East
+	};
+
+	public static MazeDirection GetOpposite (this MazeDirection direction) {
+		return opposites[(int)direction];
+	}
+}
