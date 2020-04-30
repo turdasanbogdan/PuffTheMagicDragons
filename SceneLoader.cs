9311a9630cfc5c2a9c1abcb6187cde84d7160f61
= <marisoana@yahoo.com>
2020-04-30 17:59:52 +0300

    add menu

---
+++ SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    const string CORE_GAME = "Game";
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(CORE_GAME);
+    }
+   
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game!");
+    }
+}
